13cf8a37d911b27617dfdb727c3edd521f3e548b
JefferyXie <jeffxie27@outlook.com>
2021-11-03 13:21:07 -0600

    Test commit.

--- app/src/App.js
+++ app/src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hello how ya doin
         </p>
         <a
           className="App-link"
