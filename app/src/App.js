10f4989e26c8bebb1330c453c31ca477b252e7f3
JefferyXie <jeffxie27@outlook.com>
2021-11-15 17:48:48 -0600

    Hooked up page switching to react router.

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,28 @@ import './App.css';
 
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
+import About from './Components/About'
+import Donation from './Components/Donation';
+import Contact from './Components/Contact';
+import Account from './Components/Account';
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="Hero">
       <Navbar></Navbar>
-      <Hero></Hero>
+
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Hero/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/donate" element={<Donation/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/account" element={<Account/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- app/src/Components/Navbar.js
+++ app/src/Components/Navbar.js
@@ -2,22 +2,28 @@ import '../App.css';
 import './Navbar.css';
 
 const Navbar = (props) => {
+    const switchPage = (clickedLink) => {
+        console.log(document.getElementsByClassName(clickedLink));
+        document.getElementByClassName('active').className.replace('active', '');
+        document.getElementByClassName(clickedLink).className += ' active';
+    }
+
     return (
         <nav className="App-navbar">
             <div className="App-navbar-container flex">
-                <a className="App-logo"><i class="fab fa-sith"></i>UniteTheWorld</a>
+                <a href="/" className="App-logo"><i className="fab fa-sith"></i>UniteTheWorld</a>
                 <ul className="App-navbar-menu flex">
                     <li className="App-navbar-item flex">
-                        <a className="App-navbar-link">About</a>
+                        <a href="/about" onClick={() => switchPage('about')} className="App-navbar-link about">About</a>
                     </li>
                     <li className="App-navbar-item flex">
-                        <a className="App-navbar-link">Donate</a>
+                        <a href="/donate" onClick={() => switchPage('donate')}  className="App-navbar-link donate">Donate</a>
                     </li>
                     <li className="App-navbar-item flex">
-                        <a className="App-navbar-link">Contact</a>
+                        <a href="/contact" onClick={() => switchPage('contact')}  className="App-navbar-link contact">Contact</a>
                     </li>
                     <li className="App-navbar-item flex">
-                        <a className="App-navbar-link">Account</a>
+                        <a href="/account" onClick={() => switchPage('account')}  className="App-navbar-link account">Account</a>
                     </li>
                 </ul>
             </div>
