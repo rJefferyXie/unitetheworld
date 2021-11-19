6441c362690592b0a93ce6a382bd2a54b70527a3
JefferyXie <jeffxie27@outlook.com>
2021-11-19 14:01:24 -0600

    Contact form started.

--- app/client/src/Components/Contact.js
+++ app/client/src/Components/Contact.js
@@ -1,9 +1,43 @@
 import '../App.css';
 import './Contact.css';
 
+import React, { useRef} from 'react'
+
 const Contact = (props) => {
+
+    const firstNameRef = useRef(null);
+    const lastNameRef = useRef(null);
+    const emailRef = useRef(null);
+    const phoneRef = useRef(null);
+    const messageRef = useRef(null);
+
+    const submit = (event) => {
+        event.preventDefault();
+        const data = {
+            firstName: firstNameRef.current.value,
+            lastName: lastNameRef.current.value,
+            email: emailRef.current.value,
+            phone: phoneRef.current.value,
+            message: messageRef.current.value
+        }
+        alert(JSON.stringify(data))
+    }
+
     return (
-        <div className="Contact flex">Contact</div>
+        <div className="Contact flex">
+            <form onSubmit={submit} className="Contact-form flex">
+                <div className="Name-container">
+                    <input className="Form-item" type="text" ref={firstNameRef} placeholder="First Name" tabIndex="1"></input>
+                    <input className="Form-item" type="text" ref={lastNameRef} placeholder="Last name" tabIndex="2"></input>
+                </div>
+                <div className="Email-phone-container">
+                    <input className="Form-item" type="email" ref={emailRef} placeholder="Email"tabIndex="3"></input>
+                    <input className="Form-item" type="text" ref={phoneRef} placeholder="Phone (Optional)"tabIndex="4"></input>
+                </div>
+                <textarea className="Form-item Form-message" type="text" ref={messageRef} placeholder="Type your message here" tabIndex="5"></textarea>
+                <button className="Form-item Form-button" type="submit">Send Message</button>
+            </form>
+        </div>
     );
 }
 
