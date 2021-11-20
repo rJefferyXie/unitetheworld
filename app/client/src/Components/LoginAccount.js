217d0e7a1469b7fbcef509a4b895939403283328
JefferyXie <jex862@usask.ca>
2021-11-20 16:57:54 -0600

    Login page icons added.

--- app/client/src/Components/LoginAccount.js
+++ app/client/src/Components/LoginAccount.js
@@ -2,13 +2,42 @@ import '../App.css';
 import './LoginAccount.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
 
-const LoginAccount = (props) => {
+const LoginAccount = () => {
+    const cancel = () => {
+        document.location.pathname = "/account";
+    }
+
+    const submit = (e) => {
+        e.preventDefault();
+
+    }
+
     return (
         <div className="Login flex">
-            <FontAwesomeIcon icon={faCoffee}/>
+            <form className="Create-form flex" onSubmit={submit}>
+                <h3>Login</h3>
+                <div className="Login-username">
+                    <label>Username</label>
+                    <div className="Login-item Form-item">
+                        <FontAwesomeIcon icon={faUser}/>
+                        <input className="Login-input" type="text" placeholder="Username"/>
+                    </div>
+                </div>
 
+                <div className="Login-password">
+                    <label>Password</label>
+                    <div className="Login-item Form-item">
+                        <FontAwesomeIcon icon={faLock}/>
+                        <input className="Login-input" type="text" placeholder="Password"/>
+                    </div>
+                </div>
+                <div className="Button-container flex">
+                    <button className="Form-item Form-button" type="reset"onClick={cancel}>Cancel</button>
+                    <button className="Form-item Form-button" type="submit">Login</button>
+                </div>
+            </form>
         </div>
     );
 }
